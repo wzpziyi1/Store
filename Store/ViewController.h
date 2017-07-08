24beb0d013413e1446d8175ee8e82b052013ab74
wzpziyi1 <1204473090@qq.com>
2017-07-08 13:53:46 +0800

    Initial Commit

---
+++ Store/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Store
+//
+//  Created by 王志盼 on 2017/7/8.
+//  Copyright © 2017年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Store/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Store
+//
+//  Created by 王志盼 on 2017/7/8.
+//  Copyright © 2017年 王志盼. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
